61cb3492e4283cb33a5750037534736f65e974be
Sathiyapramod <sathiyapramod22@gmail.com>
2023-03-22 16:31:35 +0530

    added URL

--- src/General.js
+++ src/General.js
@@ -1,4 +1,4 @@
-export const API = "http://localhost:4000";
-// export const API = "https://url-shortener-backend-a7h5.onrender.com";
+// export const API = "http://localhost:4000";
+export const API = "https://url-shortener-backend-a7h5.onrender.com";
 
-export const frontEndURL = "http://localhost:3000";
+export const frontEndURL = "https://password-reset-sathiyapramod.netlify.app";
